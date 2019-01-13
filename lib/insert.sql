95572f6b6195aa0dc6a05aaab2635c96e6dd2d97
canikwe <c.anikwe@gmail.com>
2019-01-13 12:36:23 -0500

    Rawr 13JAN19

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "Girl", "Brown", "Smooth", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 6, "Girl", "Orange", "Calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "Boy", "Brown", "Mysterious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "Boy", "Grey", "Haughty", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "Girl", "Black", "Grouchy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "Boy", "Green", "Genteel", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "Girl", "Red", "Salty", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 7, "GIRL", "Black", "Aggressive", 1);
